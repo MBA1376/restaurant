ca1d421af4db1fabb9d5b422d502697a113b3fb5
Mohammadbagher abbasi <mohammadbaghera5@gmail.com>
2019-04-01 14:19:14 +0430

    add react-router-dom and redux redux-thunk react-redux compose

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import {Provider} from 'react-redux';
+import store from './store';
+import {BrowserRouter as Router , Route} from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
 import GeneralPage from './components/layout/GeneralPage';
@@ -12,10 +15,15 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <GeneralPage />
-      </div>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={GeneralPage} />
+            {/* <GeneralPage /> */}
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
-import axios from 'axios';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {withRouter} from 'react-router-dom';
+import { registerUser } from '../../actions/authActions';
 
 import Modal from 'react-modal';
 
@@ -48,6 +51,12 @@ class Register_Login extends Component {
         this.onChange = this.onChange.bind(this);
     }
 
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.errors) {
+            this.setState({ errors : nextProps.errors });
+        }
+    }
+
     onSubmit(e) {
         e.preventDefault();
 
@@ -57,17 +66,13 @@ class Register_Login extends Component {
             password2 : this.state.password2
         }
 
-        axios.post('/api/users/register' , newUser)
-             .then(res => console.log(res.data))
-            .catch(err => this.setState({
-                errors : err.response.data
-            }));
+        this.props.registerUser(newUser);
         
         /**this.props.history is for redirecting to another page */
         /*this.props.registerUser(newUser , this.props.history);*/
 
     }
-
+    
     onChange(e) {
         this.setState({ [e.target.name] : e.target.value });
     }
@@ -79,7 +84,7 @@ class Register_Login extends Component {
             <Modal
                 isOpen={this.props.signupModalIsOpen}
                 // onAfterOpen={this.afterOpenModal} 
-                onRequestClose={this.props.closeNumberModal}
+                onRequestClose={this.props.closeSignupModal}
                 style={customStyles}
                 contentLabel="Example Modal"
             >
@@ -99,113 +104,87 @@ class Register_Login extends Component {
                                     </button></div>
                             </div>
                             <div className="container">
-                                    <div className="row text-center register-modal-title">
+                                <div className="row text-center register-modal-title">
+                                    <div className="col-sm-12">
+                                        <h6>برای ثبت نام شماره ایمیل و رمز عبورت رو وارد کن</h6>
+                                    </div>
+                                </div>
+                                <form noValidate onSubmit={this.onSubmit}>
+                                    <div className="row text-center">
+                                        
                                         <div className="col-sm-12">
-                                            <h6>برای ثبت نام شماره ایمیل و رمز عبورت رو وارد کن</h6>
+                                            <input 
+                                                className = {classnames('login-input form-control' , {
+                                                    'is-invalid' : errors.email
+                                                })}
+                                                placeholder="ایمیل" 
+                                                name="email"
+                                                type="email"
+                                                value={this.state.email}
+                                                onChange = {this.onChange}
+                                            />
                                         </div>
                                     </div>
-                                    <form noValidate onSubmit={this.onSubmit}>
-                                        <div className="row text-center">
-                                            
-                                            <div className="col-sm-12">
-                                                <input 
-                                                    className = {classnames('login-input form-control' , {
-                                                        'is-invalid' : errors.email
-                                                    })}
-                                                    placeholder="ایمیل" 
-                                                    name="email"
-                                                    type="email"
-                                                    value={this.state.email}
-                                                    onChange = {this.onChange}
-                                                />
-                                            </div>
+                                    <div className="row text-center">
+                                        <div className="col-sm-12">
+                                            <input 
+                                                className={classnames('login-input form-control' , {
+                                                    'is-invalid' : errors.password
+                                                })}
+                                                placeholder="پسورد"
+                                                name="password"
+                                                type="password"
+                                                value={this.state.password}
+                                                onChange={this.onChange}
+                                            />
                                         </div>
-                                        <div className="row text-center">
-                                            <div className="col-sm-12">
-                                                <input 
-                                                    className={classnames('login-input form-control' , {
-                                                        'is-invalid' : errors.password
-                                                    })}
-                                                    placeholder="پسورد"
-                                                    name="password"
-                                                    type="password"
-                                                    value={this.state.password}
-                                                    onChange={this.onChange}
-                                                />
-                                            </div>
+                                    </div>
+                                    <div className="row text-center">
+                                        <div className="col-sm-12">
+                                            <input 
+                                                className={classnames('login-input form-control' , {
+                                                    'is-invalid' : errors.password2
+                                                })}
+                                                placeholder="تایید پسورد"
+                                                name="password2"
+                                                type="password"
+                                                value={this.state.password2}
+                                                onChange={this.onChange}
+                                            />
                                         </div>
-                                        <div className="row text-center">
-                                            <div className="col-sm-12">
-                                                <input 
-                                                    className={classnames('login-input form-control' , {
-                                                        'is-invalid' : errors.password2
-                                                    })}
-                                                    placeholder="تایید پسورد"
-                                                    name="password2"
-                                                    type="password"
-                                                    value={this.state.password2}
-                                                    onChange={this.onChange}
-                                                />
-                                            </div>
+                                    </div>
+                                    <div className="row text-center">
+                                        <div className="col-sm-12">
+                                            <button 
+                                                // onClick={this.props.openLoginModal}
+                                                className="button login-btn"
+                                                type="submit"
+                                            >
+                                                ثبت نام
+                                            </button>
                                         </div>
-                                        <div className="row text-center">
-                                            <div className="col-sm-12">
-                                                <button 
-                                                    // onClick={this.props.openLoginModal}
-                                                    className="button login-btn"
-                                                    type="submit"
-                                                >
-                                                    ثبت نام
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </form>
+                                    </div>
+                                </form>
                             </div>                                 
                         </div>
                     </div>
                 </div>
             </Modal>
-    )
-  }
-}
+            )
+        }
+    }
 
-export default Register_Login;
+    PropTypes.registerUser = {
+        registerUser : PropTypes.func.isRequired ,
+        auth : PropTypes.object.isRequired,
+        errors : PropTypes.object.isRequired
+    };
 
- {/* <Modal
-                    isOpen={this.props.loginModalIsOpen}
-                    // onAfterOpen={this.afterOpenModal} 
-                    onRequestClose={this.props.closeLoginModal}
-                    style={customStyles}
-                    contentLabel="Example Modal"
-                >
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-sm-5 modal-sidebar"></div>
-                            <div className="col-sm-7">
-                                <div className="row">
-                                    <div className="col-sm-10"><button onClick={this.props.closeLoginModal} style={this.backButtonStyle} className="button"><img src={rBack}></img></button></div>
-                                    <div className="col-sm-2"></div>
-                                </div>
-                                <div className="container">
-                                        <div className="row text-center modal-title">
-                                            <div className="col-sm-12">
-                                                <h6>رمز عبورت رو وارد کن</h6>
-                                            </div>
-                                        </div>
-                                        <div className="row text-center">
-                                            <div className="col-sm-12">
-                                                <input className="login-input" />
-                                            </div>
-                                        </div>
-                                        <div className="row text-center">
-                                            <div className="col-sm-12">
-                                                <button className="button login-btn">
-                                                     ورود 
-                                                </button>
-                                            </div>
-                                        </div>
-                                </div>                                 
-                            </div>
-                        </div>
-                    </div>
-                </Modal> */}
+    const mapStateToProps = state => ({
+        auth : state.auth ,
+        errors : state.errors
+    })
+
+    export default connect(mapStateToProps , {registerUser})(withRouter(Register_Login)) ;
+
+ 
